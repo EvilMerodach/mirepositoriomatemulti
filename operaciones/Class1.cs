f3884d3bc5af961958c7f9c18caf5b09ca6dd943
EvilMerodach <125280518+EvilMerodach@users.noreply.github.com>
2024-05-01 19:12:52 -0500

    mi commit de multiplicacion

---
+++ operaciones/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace operaciones;
+
+public class operacionesbasicas
+{
+    public int multiplicacion(int a, int b)
+    {
+        return a * b;
+    }
+}
---
+++ testoperaciones/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿namespace testoperaciones;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(2, 3, 6)]
+    [InlineData(11, 4, 44)]
+    [InlineData(4, 1, 4)]
+    [InlineData(8, 2, 16)]
+    [InlineData(11, 2, 22)]
+    public void testmultiplicacion(int a, int b, int resultadoesperado)
+    {
+        ///AAA Arange Act Assert
+        ///Arange
+        int resultadoreal;
+        ///Act
+        resultadoreal=operacionesbasicas.multiplicacion(a,b);
+        ///Assert
+        Assert.Equal(resultadoesperado, resultadoreal)
+
+    }
+}
